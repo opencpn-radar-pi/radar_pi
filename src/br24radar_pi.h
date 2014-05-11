afc37a32e39c072a9584ec90d47eb7c03f4a7615
Kees Verruijt <kees@verruijt.net>
2014-05-11 22:59:04 +0200

    Version 0.40511 for controls v3 rewrite

--- src/br24radar_pi.h
+++ src/br24radar_pi.h
@@ -39,7 +39,7 @@
 #endif //precompiled headers
 
 #define     PLUGIN_VERSION_MAJOR    0
-#define     PLUGIN_VERSION_MINOR    40510
+#define     PLUGIN_VERSION_MINOR    40511
 
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    8
