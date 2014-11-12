45caa635a66f1bf0f8ba363c5db2a04550070699
Kees Verruijt <kees@verruijt.net>
2014-11-12 15:49:53 +0100

    Release 1.0

--- src/br24radar_pi.h
+++ src/br24radar_pi.h
@@ -39,7 +39,7 @@
 #endif //precompiled headers
 
 #define     PLUGIN_VERSION_MAJOR    1
-#define     PLUGIN_VERSION_MINOR    41110
+#define     PLUGIN_VERSION_MINOR    0
 
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    8
