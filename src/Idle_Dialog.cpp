4e26b54921371aa20c443a48f1074fca09fedfdc
Hakansv <momo@stupan.se>
2015-05-11 19:24:23 +0200

    Changed Idle_dialog gauge. Moved Timed_Idle setting out of config file

--- src/Idle_Dialog.cpp
+++ src/Idle_Dialog.cpp
@@ -126,7 +126,7 @@ void Idle_Dialog::CreateControls()
 	p_IdleTimeLeft->Wrap( -1 );
 	sbIdleDialogSizer->Add( p_IdleTimeLeft, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, border );
 
-	m_Idle_gauge = new wxGauge( this, wxID_ANY, 100, wxDefaultPosition, wxDefaultSize, wxGA_HORIZONTAL);
+	m_Idle_gauge = new wxGauge( this, wxID_ANY, 100, wxDefaultPosition, wxSize( -1,12 ), wxGA_HORIZONTAL);
 	m_Idle_gauge->SetValue( 0 ); 
 	sbIdleDialogSizer->Add( m_Idle_gauge, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, border);
 	
