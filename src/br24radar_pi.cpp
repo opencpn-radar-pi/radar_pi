66d97ab2fc0a1209922720fa3169455617303017
Douwe Fokkema <df@percussion.nl>
2016-03-05 16:46:33 +0100

    Version Beta 1.304

--- src/br24radar_pi.cpp
+++ src/br24radar_pi.cpp
@@ -511,6 +511,7 @@ int br24radar_pi::Init(void)
             polar_to_cart_x[arc][radius] = (GLfloat) radius * cosine;
         }
     }
+    wxLogMessage(wxT("BR24radar_pi: Beta Version 1.304"));
     wxLogMessage(wxT("BR24radar_pi:Position initialized  xa = %f"), polar_to_cart_x[100][150]);
     AddLocaleCatalog( _T("opencpn-br24radar_pi") );
 
