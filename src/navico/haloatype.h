15bd92bffa577504703b57157741518956c9d217
Douwe Fokkema <df@percussion.nl>
2017-10-30 17:40:05 +0100

    Halo ip and port addresses updated. Halo giving radar image, needs further testing

--- src/navico/haloatype.h
+++ src/navico/haloatype.h
@@ -5,9 +5,9 @@ PLUGIN_BEGIN_NAMESPACE
 
 // Note that the order of the ports is different on A and B. I guess someone
 // in Navico just didn't realize this. Or it is just a bit of obfuscation.
-static const NetworkAddress dataHalo_A(236, 6, 7, 8, 6678);
-static const NetworkAddress reportHalo_A(236, 6, 7, 9, 6679);
-static const NetworkAddress sendHalo_A(236, 6, 7, 10, 6680);
+static const NetworkAddress dataHalo_A(236, 6, 7, 100, 6132);
+static const NetworkAddress reportHalo_A(236, 6, 7, 102, 6134);
+static const NetworkAddress sendHalo_A(236, 6, 7, 101, 6133);
 
 PLUGIN_END_NAMESPACE
 
--- src/navico/halobtype.h
+++ src/navico/halobtype.h
@@ -5,9 +5,9 @@ PLUGIN_BEGIN_NAMESPACE
 
 // Note that the order of the ports is different on A and B. I guess someone
 // in Navico just didn't realize this. Or it is just a bit of obfuscation.
-static const NetworkAddress dataHalo_B(236, 6, 7, 8, 6678);
-static const NetworkAddress reportHalo_B(236, 6, 7, 9, 6679);
-static const NetworkAddress sendHalo_B(236, 6, 7, 10, 6680);
+static const NetworkAddress dataHalo_B(236, 6, 7, 103, 6135);
+static const NetworkAddress reportHalo_B(236, 6, 7, 105, 6137);
+static const NetworkAddress sendHalo_B(236, 6, 7, 104, 6136);
 
 PLUGIN_END_NAMESPACE
 
