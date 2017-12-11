469143473e6d6ccb8b9e638f222b9b53dacc56fa
Kees Verruijt <kees@verruijt.net>
2017-12-11 18:48:36 +0100

    Issue #25. Only stop TTX mode when pressing power when the timer is already running.

--- src/ControlsDialog.cpp
+++ src/ControlsDialog.cpp
@@ -1425,7 +1425,12 @@ void ControlsDialog::OnRadarGainButtonClick(wxCommandEvent& event) { EnterEditMo
 void ControlsDialog::OnTransmitButtonClick(wxCommandEvent& event) {
   RadarState state = (RadarState)m_ri->m_state.GetButton();
   SetMenuAutoHideTimeout();
-  m_timed_idle_button->SetState(RCS_OFF);
+
+  // If we already have a running timer, then turn timed_idle_mode off
+  if (m_ri->m_next_state_change.GetValue() > 1 &&
+      (m_ri->m_timed_idle_hardware || m_ri->m_idle_transmit > 0 || m_ri->m_idle_standby > 0)) {
+    m_timed_idle_button->SetState(RCS_OFF);
+  }
   if (state == RADAR_STANDBY || state == RADAR_STOPPING || state == RADAR_SPINNING_DOWN) {
     m_ri->RequestRadarState(RADAR_TRANSMIT);
   } else {
