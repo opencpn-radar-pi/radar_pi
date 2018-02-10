f75b5fc5a9e9c546eb7d4c57672a8f3d45310d6b
Rasbats <miker@members.swis.net>
2018-02-10 11:34:14 +0000

    Another text case problem for Linux! Lesson ... use lower case consistently
    Signed-off-by: Rasbats

--- src/AisMaker.cpp
+++ src/AisMaker.cpp
@@ -25,7 +25,7 @@
 ***************************************************************************
 */
 
-#include "shipdrivergui_impl.h"
+#include "ShipDrivergui_impl.h"
 
 static const std::vector<std::pair<uint32_t, char>> payloadencoding = {
 	{ 0, '0' }, { 1, '1' }, { 2, '2' }, { 3, '3' }, { 4, '4' }, { 5, '5' }, { 6, '6' }, { 7, '7' }, { 8, '8' }, { 9, '9' }, { 10, ':' }, { 11, ';' },
