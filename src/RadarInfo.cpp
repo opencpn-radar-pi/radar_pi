opencpn-radar-pi/radar_pi

commit 1292d79558d7b6ff60a285b014c17ef0b11f9484

Adaptation of context menu for radar control. Context menu will be dynamically updated with change of radar types

src/RadarInfo.cpp
@@ -167,6 +167,10 @@ void RadarInfo::Shutdown() {
 RadarInfo::~RadarInfo() {
   Shutdown();
 
+// delete context menu item for radar control
+// it does not harm to remove an non existing menu
+  RemoveCanvasContextMenuItem(m_pi->m_context_menu_control_id[m_radar]);
+  
   if (m_draw_panel.draw) {
     delete m_draw_panel.draw;
     m_draw_panel.draw = 0;
@@ -216,7 +220,7 @@ bool RadarInfo::Init() {
   m_name = RadarTypeName[m_radar_type];
   m_spokes = RadarSpokes[m_radar_type];
   m_spoke_len_max = RadarSpokeLenMax[m_radar_type];
-
+  
   m_history = (line_history *)calloc(sizeof(line_history), m_spokes);
   for (size_t i = 0; i < m_spokes; i++) {
     m_history[i].line = (uint8_t *)calloc(sizeof(uint8_t), m_spoke_len_max);
@@ -227,6 +231,15 @@ bool RadarInfo::Init() {
 
   if (!m_control) {
     m_control = RadarFactory::MakeRadarControl(m_radar_type);
+    // add context menu for control
+    wxString t;
+    wxMenu dummy_menu;
+    wxFont *qFont = OCPNGetFont(_("Menu"), 10);
+    t = _("");
+    t << _("Control ") << m_name;
+    m_pi->m_mi3[m_radar] = new wxMenuItem(&dummy_menu, -1, t);
+    m_pi->m_mi3[m_radar]->SetFont(*qFont);
+    m_pi->m_context_menu_control_id[m_radar] = AddCanvasContextMenuItem(m_pi->m_mi3[m_radar], m_pi);
   }
   if (!m_radar_panel) {
     m_radar_panel = new RadarPanel(m_pi, this, GetOCPNCanvasWindow());

src/radar_pi.cpp
@@ -297,14 +297,6 @@ int radar_pi::Init(void) {
 
   wxMenuItem *mi1 = new wxMenuItem(&dummy_menu, -1, _("Show radar"));
   wxMenuItem *mi2 = new wxMenuItem(&dummy_menu, -1, _("Hide radar"));
-  wxMenuItem *mi3[RADARS];
-  if (M_SETTINGS.radar_count > RADARS) M_SETTINGS.radar_count = RADARS;
-  for (size_t r = 0; r < M_SETTINGS.radar_count; r++) {
-    wxString t;
-    t =  _("");
-    t << _("Control ") << m_radar[r]->m_name.c_str();
-    mi3[r] = new wxMenuItem(&dummy_menu, -1, t);
-  }
 
   wxMenuItem *mi4 = new wxMenuItem(&dummy_menu, -1, _("Acquire radar target"));
   wxMenuItem *mi5 = new wxMenuItem(&dummy_menu, -1, _("Delete radar target"));
@@ -315,7 +307,7 @@ int radar_pi::Init(void) {
   mi1->SetFont(*qFont);
   mi2->SetFont(*qFont);
   for (size_t r = 0; r < M_SETTINGS.radar_count; r++) {
-    mi3[r]->SetFont(*qFont);
+    m_mi3[r]->SetFont(*qFont);
   }
   mi4->SetFont(*qFont);
   mi5->SetFont(*qFont);
@@ -323,14 +315,10 @@ int radar_pi::Init(void) {
 #endif
   m_context_menu_show_id = AddCanvasContextMenuItem(mi1, this);
   m_context_menu_hide_id = AddCanvasContextMenuItem(mi2, this);
-  for (size_t r = 0; r < M_SETTINGS.radar_count; r++) {
-    m_context_menu_control_id[r] = AddCanvasContextMenuItem(mi3[r], this);
-  }
   m_context_menu_acquire_radar_target = AddCanvasContextMenuItem(mi4, this);
   m_context_menu_delete_radar_target = AddCanvasContextMenuItem(mi5, this);
   m_context_menu_delete_all_radar_targets = AddCanvasContextMenuItem(mi6, this);
   m_context_menu_show = true;
-  m_context_menu_control = false;
   m_context_menu_arpa = false;
   SetCanvasContextMenuItemViz(m_context_menu_show_id, false);
 
@@ -443,6 +431,7 @@ bool radar_pi::IsRadarSelectionComplete(bool force) {
     r = 0;
     for (size_t i = 0; i < RT_MAX; i++) {
       if (dlg.m_selected[i]->GetValue()) {
+        wxFont *qFont = OCPNGetFont(_("Menu"), 10);
         if (!m_radar[r]) {
           m_settings.window_pos[r] = wxPoint(100 + 512 * r, 100);
           m_settings.control_pos[r] = wxDefaultPosition;
@@ -459,6 +448,7 @@ bool radar_pi::IsRadarSelectionComplete(bool force) {
     for (r = 0; r < M_SETTINGS.radar_count; r++) {
       if (m_radar[r] && m_radar[r]->m_radar_type != oldRadarType[r]) {
         m_radar[r]->Shutdown();
+        RemoveCanvasContextMenuItem(m_context_menu_control_id[r]);
         delete m_radar[r];
         m_radar[r] = new RadarInfo(this, r);
       }
@@ -476,6 +466,7 @@ bool radar_pi::IsRadarSelectionComplete(bool force) {
         m_radar[r] = 0;
       }
     }
+    
     SetRadarWindowViz();
     TimedControlUpdate();
   }
@@ -974,6 +965,9 @@ void radar_pi::TimedControlUpdate() {
   // in dual canvas run SetRadarWindowViz only on canvas 1,
   // otherwise crash in ShowFrame on m_aui_mgr->Update(); line 225
   // if (current_canvas != m_canvas[1] && m_max_canvas > 0) return;
+  /*if (m_max_canvas > 0) {   // new version of crash prevention?
+    if (current_canvas != GetCanvasByIndex(1)) return;
+  }*/
   m_notify_time_ms = now;
 
   bool updateAllControls = m_notify_control_dialog;

src/radar_pi.h
@@ -487,6 +487,7 @@ class radar_pi : public opencpn_plugin_116, public wxEvtHandler {
   bool m_guard_bogey_confirmed;
   bool m_guard_bogey_seen;  // Saw guardzone bogeys on last check
   int m_max_canvas;         // Number of canvasses in OCPN -1, 0 == single canvas, > 0  multi
+  wxMenuItem *m_mi3[RADARS];
   PlugIn_ViewPort *m_vp;
 
   wxFont m_font;      // The dialog font at a normal size
@@ -597,7 +598,6 @@ class radar_pi : public opencpn_plugin_116, public wxEvtHandler {
 
   // Keep last state of ContextMenu state sent, to avoid redraws
   bool m_context_menu_show;
-  bool m_context_menu_control;
   bool m_context_menu_arpa;
 
   // Cursor position. Used to show position in radar window

commit f606b785bad136d6081de4582b3329703002bd55

Fix crash introduced in 1292d79558d7b6ff60a285b014c17ef0b11f9484

src/RadarInfo.cpp
@@ -167,10 +167,10 @@ void RadarInfo::Shutdown() {
 RadarInfo::~RadarInfo() {
   Shutdown();
 
-// delete context menu item for radar control
-// it does not harm to remove an non existing menu
+  // delete context menu item for radar control
+  // it does not harm to remove an non existing menu
   RemoveCanvasContextMenuItem(m_pi->m_context_menu_control_id[m_radar]);
-  
+
   if (m_draw_panel.draw) {
     delete m_draw_panel.draw;
     m_draw_panel.draw = 0;
@@ -220,7 +220,7 @@ bool RadarInfo::Init() {
   m_name = RadarTypeName[m_radar_type];
   m_spokes = RadarSpokes[m_radar_type];
   m_spoke_len_max = RadarSpokeLenMax[m_radar_type];
-  
+
   m_history = (line_history *)calloc(sizeof(line_history), m_spokes);
   for (size_t i = 0; i < m_spokes; i++) {
     m_history[i].line = (uint8_t *)calloc(sizeof(uint8_t), m_spoke_len_max);
@@ -234,11 +234,15 @@ bool RadarInfo::Init() {
     // add context menu for control
     wxString t;
     wxMenu dummy_menu;
-    wxFont *qFont = OCPNGetFont(_("Menu"), 10);
     t = _("");
     t << _("Control ") << m_name;
     m_pi->m_mi3[m_radar] = new wxMenuItem(&dummy_menu, -1, t);
+
+#ifdef __WXMSW__
+    wxFont *qFont = OCPNGetFont(_("Menu"), 10);
     m_pi->m_mi3[m_radar]->SetFont(*qFont);
+#endif
+
     m_pi->m_context_menu_control_id[m_radar] = AddCanvasContextMenuItem(m_pi->m_mi3[m_radar], m_pi);
   }
   if (!m_radar_panel) {

src/radar_pi.cpp
@@ -306,13 +306,11 @@ int radar_pi::Init(void) {
   wxFont *qFont = OCPNGetFont(_("Menu"), 10);
   mi1->SetFont(*qFont);
   mi2->SetFont(*qFont);
-  for (size_t r = 0; r < M_SETTINGS.radar_count; r++) {
-    m_mi3[r]->SetFont(*qFont);
-  }
   mi4->SetFont(*qFont);
   mi5->SetFont(*qFont);
   mi6->SetFont(*qFont);
 #endif
+
   m_context_menu_show_id = AddCanvasContextMenuItem(mi1, this);
   m_context_menu_hide_id = AddCanvasContextMenuItem(mi2, this);
   m_context_menu_acquire_radar_target = AddCanvasContextMenuItem(mi4, this);