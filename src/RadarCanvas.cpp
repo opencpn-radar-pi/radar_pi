opencpn-radar-pi/radar_pi

commit bd581566f4d0eb51ce63170110fa639ad8abdb2c

Solution for the "rollover problem" Rollover problem is that the AIS target mouse rollover info is distorted when radar window is open.

src/RadarCanvas.cpp
@@ -679,6 +679,7 @@ void RadarCanvas::Render(wxPaintEvent &evt) {
 
   glPopAttrib();
   glPopMatrix();
+  SetCurrent(*m_pi->m_opencpn_gl_context);
   SwapBuffers();
 
 #ifdef CRASH

src/radar_pi.h
@@ -690,6 +690,7 @@ class radar_pi : public opencpn_plugin_116, public wxEvtHandler {
   bool m_predicted_position_initialised;
   ExtendedPosition m_expected_position;  // updated own position at time of last GPS update
   ExtendedPosition m_last_fixed;         // best estimate position at last measurement
+  wxGLContext *m_opencpn_gl_context;
  private:
   bool m_initialized;      // True if Init() succeeded and DeInit() not called yet.
   bool m_first_init;       // True in first Init() call.
@@ -698,7 +699,6 @@ class radar_pi : public opencpn_plugin_116, public wxEvtHandler {
   OpenGLMode m_opengl_mode;
   volatile bool m_opengl_mode_changed;
 
-  wxGLContext *m_opencpn_gl_context;
   bool m_opencpn_gl_context_broken;
 
   wxTimer *m_timer;

commit debd1ce314db2f322a6ac903570b2eb263f31a51

Repair macOS drawing artifacts
... caused by commit bd581566f4d0eb51ce63170110fa639ad8abdb2c

src/RadarCanvas.cpp
@@ -679,17 +679,27 @@ void RadarCanvas::Render(wxPaintEvent &evt) {
 
   glPopAttrib();
   glPopMatrix();
-  SetCurrent(*m_pi->m_opencpn_gl_context);
+
+  // Do the swapbuffers first, before restoring the context. If we don't then various artifacts
+  // occur on MacOS with the radar PPI window getting completely distorted.
+  // Also it seems much more logical to call SwapBuffers() *before* going back to the OpenCPN
+  // context.
   SwapBuffers();
 
-#ifdef CRASH
+  // Restore the OpenGL context, so that AIS rollover doesn't break.
+  // Apparently this is executed in a timer on Windows and Linux in such
+  // a way that it is using the radar's OpenGL context.
+  //
+  // This used to break on Mac, where timing is different and we are not
+  // allowed to set the context from a class that is derived from a window
+  // where the context is for a different window, but with wx 3.1.2 + OpenCPN
+  // patches this seems to be fixed.
   wxGLContext *chart_context = m_pi->GetChartOpenGLContext();
   if (chart_context) {
     SetCurrent(*chart_context);
   } else {
     SetCurrent(*m_zero_context);  // Make sure OpenCPN -at least- doesn't overwrite our context info
   }
-#endif
 }
 
 void RadarCanvas::OnMouseMotion(wxMouseEvent &event) {

src/radar_pi.h
@@ -690,7 +690,6 @@ class radar_pi : public opencpn_plugin_116, public wxEvtHandler {
   bool m_predicted_position_initialised;
   ExtendedPosition m_expected_position;  // updated own position at time of last GPS update
   ExtendedPosition m_last_fixed;         // best estimate position at last measurement
-  wxGLContext *m_opencpn_gl_context;
  private:
   bool m_initialized;      // True if Init() succeeded and DeInit() not called yet.
   bool m_first_init;       // True in first Init() call.
@@ -699,6 +698,7 @@ class radar_pi : public opencpn_plugin_116, public wxEvtHandler {
   OpenGLMode m_opengl_mode;
   volatile bool m_opengl_mode_changed;
 
+  wxGLContext *m_opencpn_gl_context;
   bool m_opencpn_gl_context_broken;
 
   wxTimer *m_timer;