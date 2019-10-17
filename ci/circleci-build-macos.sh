bd8d898db130a395987de06010d6505a3d15b5a6
Alec Leamas <leamas.alec@gmail.com>
2019-10-17 21:31:31 +0200

    ci: circleci-build-macos: Clean up log clutter.

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -6,11 +6,12 @@
 
 set -xe
 
+set -o pipefail
 for pkg in cairo libexif xz libarchive python3 wget cmake; do
-    brew list $pkg 2>&1 >/dev/null || brew install $pkg
+    brew list $pkg 2>/dev/null | head -10 || brew install $pkg
 done
 
-wget http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
+wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
 tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
