71b7a9d2136c0cc8aa4dd95538642e8ea4b24555
Kees Verruijt <kees@verruijt.net>
2020-05-23 14:51:16 -0400

    Add architecture descriptor to Linux package names

--- cmake/PluginSetup.cmake
+++ cmake/PluginSetup.cmake
@@ -42,18 +42,40 @@ elseif (APPLE)
 elseif (UNIX)
     # Some linux dist:
     execute_process(COMMAND "lsb_release" "-is"
-                    OUTPUT_VARIABLE PKG_TARGET)
+                    OUTPUT_VARIABLE PKG_TARGET
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND "lsb_release" "-rs"
-                    OUTPUT_VARIABLE PKG_TARGET_VERSION)
+                    OUTPUT_VARIABLE PKG_TARGET_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    # Handle gtk3 build variant                
+    if (NOT DEFINED wxWidgets_LIBRARIES)
+        message(FATAL_ERROR "PluginSetup: required wxWidgets_LIBRARIES missing")
+    elseif ("${wxWidgets_LIBRARIES}" MATCHES "gtk3u" AND PKG_TARGET MATCHES "ubuntu*")
+        set(PKG_TARGET "${PKG_TARGET}-gtk3")
+    endif ()
+
+    # Generate architecturally uniques names for linux output packages
+    if(ARCH MATCHES "arm64")
+        set(PKG_TARGET "${PKG_TARGET}-ARM64")
+    elseif(ARCH MATCHES "armhf")
+        set(PKG_TARGET "${PKG_TARGET}-ARMHF")
+    elseif(ARCH MATCHES "i386")
+        set(PKG_TARGET "${PKG_TARGET}-i386")
+    else ()
+        set(PKG_TARGET "${PKG_TARGET}-x86_64")
+    endif ()
 else ()
     set(PKG_TARGET "unknown")
     set(PKG_TARGET_VERSION 1)
 endif ()
 
 
-string(STRIP ${PKG_TARGET} PKG_TARGET)
-string(TOLOWER ${PKG_TARGET} PKG_TARGET)
-string(STRIP ${PKG_TARGET_VERSION} PKG_TARGET_VERSION)
-string(TOLOWER ${PKG_TARGET_VERSION} PKG_TARGET_VERSION)
-set(PKG_TARGET_NVR ${PKG_TARGET}-${PKG_TARGET_VERSION})
+string(STRIP "${PKG_TARGET}" PKG_TARGET)
+string(TOLOWER "${PKG_TARGET}" PKG_TARGET)
+string(STRIP "${PKG_TARGET_VERSION}" PKG_TARGET_VERSION)
+string(TOLOWER "${PKG_TARGET_VERSION}" PKG_TARGET_VERSION)
+set(PKG_TARGET_NVR "${PKG_TARGET}-${PKG_TARGET_VERSION}")
 
+message(STATUS "PluginSetup: PKG_TARGET: ${PKG_TARGET}")
+message(STATUS "PluginSetup: PKG_TARGET_VERSION: ${PKG_TARGET_VERSION}")
