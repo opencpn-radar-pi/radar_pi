de59ea6dcf45ad1ab3f9f980755837280f2ffce9
Alec Leamas <leamas.alec@nowhere.net>
2020-08-23 13:58:51 +0200

    build: PluginConfigure: Refactor architecture detection.
    While on it, remove unused RPM definitions

---
+++ cmake/GetArch.cmake
@@ -0,0 +1,35 @@
+# Set ARCH using cmake probes and various heuristics.
+
+if (COMMAND GetArch)
+  return ()
+endif ()
+
+# Based on code from nohal
+function(GetArch)
+  if (NOT WIN32)
+    # default
+    set (ARCH "x86_64")
+    if (CMAKE_SYSTEM_PROCESSOR MATCHES "arm*")
+      if (CMAKE_SIZEOF_VOID_P MATCHES "8")
+        set (ARCH "arm64")
+      else ()
+        set (ARCH "armhf")
+      endif ()
+    else (CMAKE_SYSTEM_PROCESSOR MATCHES "arm*")
+      set (ARCH ${CMAKE_SYSTEM_PROCESSOR})
+    endif ()
+    if (EXISTS /etc/redhat-release)
+      if (ARCH STREQUAL "arm64")
+        set (ARCH "aarch64")
+      endif()
+    elseif (EXISTS /etc/suse-release OR EXISTS /etc/SuSE-release)
+      if (ARCH STREQUAL "arm64")
+         set (ARCH "aarch64")
+      endif()
+    endif ()
+  else (NOT WIN32)
+    # On WIN32 probably CMAKE_SIZEOF_VOID_P EQUAL 8, but we don't use it at all now...
+    set (ARCH "i386")
+  endif (NOT WIN32)
+  set (ARCH ${ARCH} PARENT_SCOPE)
+endfunction(GetArch)
