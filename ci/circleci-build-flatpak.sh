156576eeff63cae7de54a5d91e12436c5519e9ee
Alec Leamas <leamas.alec@nowhere.net>
2020-08-26 07:41:11 +0200

    ci: ci/circleci-build-flatpak: Cleanup, fix build number.

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -15,37 +15,33 @@ set -xe
 sudo systemctl stop apt-daily.service apt-daily.timer
 sudo systemctl kill --kill-who=all apt-daily.service
 
-PLUGIN=bsb4
-
 DOCKER_SOCK="unix:///var/run/docker.sock"
 TOPDIR=/root/project
 
 echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s devicemapper\"" \
     | sudo tee /etc/default/docker > /dev/null
-sudo service docker restart
-sleep 5
-sudo docker pull fedora:30;
-sleep 2
+sudo systemctl restart docker.service
+sudo docker pull fedora:30
+
 docker run --privileged -d -ti -e "container=docker"  \
     -e "TOPDIR=$TOPDIR" \
     -e "CLOUDSMITH_STABLE_REPO=$CLOUDSMITH_STABLE_REPO" \
     -e "CLOUDSMITH_UNSTABLE_REPO=$CLOUDSMITH_UNSTABLE_REPO" \
+    -e "CIRCLE_BUILD_NUM=$CIRCLE_BUILD_NUM" \
     -v /sys/fs/cgroup:/sys/fs/cgroup \
     -v $(pwd):$TOPDIR:rw \
     fedora:30   /bin/bash
-DOCKER_CONTAINER_ID=$(docker ps | grep fedora | awk '{print $1}')
+DOCKER_CONTAINER_ID=$(docker ps | awk '/fedora/ {print $1}')
 docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "bash -xe $TOPDIR/ci/docker-build-flatpak.sh 28;
-         echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
+
+docker exec -ti $DOCKER_CONTAINER_ID \
+    /bin/bash -xec "bash -xe $TOPDIR/ci/docker-build-flatpak.sh"
 docker ps -a
 docker stop $DOCKER_CONTAINER_ID
 docker rm -v $DOCKER_CONTAINER_ID
 
-ps -ef | grep apt
 sudo systemctl kill --kill-who=all --signal KILL apt-daily.service
 sudo rm -f /var/lib/dpkg/lock
 sudo apt-get -q update
 sudo apt-get install python3-pip python3-setuptools
-
 pip3 install cloudsmith-cli
