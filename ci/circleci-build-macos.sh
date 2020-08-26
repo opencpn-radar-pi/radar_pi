53e30e6e67276b3f0b77a63aa5d67cc2f4fc45fd
Alec Leamas <leamas.alec@nowhere.net>
2020-08-26 14:43:26 +0200

    ci: circleci-build-macos: Fix complicated ruby update, cleanup.

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -1,46 +1,36 @@
 #!/usr/bin/env bash
 
 #
-# Build the Travis OSX artifacts 
+# Build the Travis MacOS artifacts
 #
 
-# bailout on errors and echo commands
 set -xe
 
-# As of travis-ci-macos-10.13-xcode9.4.1-1529955246, the travis osx image
-# contains a broken homebrew. Walk-around by reinstalling:
-curl -fsSL \
-     https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh \
-    > uninstall.sh
-chmod 755 uninstall.sh
-./uninstall.sh -q -f
+# Install updated ruby, system's is too old. Installations
+# fails, but seems to work anyway at last.
+brew install ruby || brew install ruby || :
+export PATH="/usr/local/opt/ruby/bin:$PATH"
 
-inst="https://raw.githubusercontent.com/Homebrew/install/master/install"
-/usr/bin/ruby -e "$(curl -fsSL $inst)"
-
-
-set -o pipefail
 for pkg in cairo cmake libarchive libexif python3 wget; do
-    brew list $pkg 2>&1 >/dev/null || brew install $pkg
+    brew list $pkg >/dev/null 2>&1 || brew install $pkg
 done
 
 wget http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
 tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
 export PATH="/usr/local/opt/gettext/bin:$PATH"
-echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
- 
+
 rm -rf build && mkdir build && cd build
-CI_BUILD=ON
-cmake -DOCPN_CI_BUILD=$CI_BUILD \
-  -DOCPN_USE_LIBCPP=ON \
+cmake \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
   -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
-  -DCMAKE_INSTALL_PREFIX= "/" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+  -DCMAKE_INSTALL_PREFIX= "/" \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
   ..
 make -sj2
 make package
 make repack-tarball
 
+# Install cloudsmith-cli, required by upload script.
 sudo -H python3 -m ensurepip
 sudo -H python3 -m pip install -q setuptools
 sudo -H python3 -m pip install -q cloudsmith-cli
