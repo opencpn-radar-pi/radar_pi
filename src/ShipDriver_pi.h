936bd41c5a40d7ddc0414b62aff99582912ae651
Alec Leamas <leamas.alec@nowhere.net>
2020-08-28 13:43:55 +0200

    libs: jsoncpp: Update and bundle in separate dir (#33).
    The jsoncpp file generates some nasty warnings. The basic
    message is that the files are outdated.
    
    Adding external libraries in src/ as actually a bad habit,
    making updates and tracking of dependencies harder. Use
    the model from main opencpn with a separate libs dir, and
    use cmake's interface libraries to handle it. Add a fresh
    jsoncpp copy, upstream commit 96e9cda.
    
    And, not least, include the tests.

--- src/ShipDriver_pi.h
+++ src/ShipDriver_pi.h
@@ -39,8 +39,8 @@
 
 #include "ocpn_plugin.h" //Required for OCPN plugin functions
 #include "ShipDrivergui_impl.h"
-#include "jsoncpp/json/reader.h"
-#include "jsoncpp/json/writer.h"
+#include "json/reader.h"
+#include "json/writer.h"
 #include <wx/datetime.h>
 #include "GribRecordSet.h"
 #include <wx/tokenzr.h>
--- src/ShipDrivergui_impl.h
+++ src/ShipDrivergui_impl.h
@@ -41,8 +41,8 @@
 #include <cmath>
 #include <wx/stdpaths.h>
 #include "wx/process.h"
-#include "jsoncpp/json/reader.h"
-#include "jsoncpp/json/writer.h"
+#include "json/reader.h"
+#include "json/writer.h"
 #include <wx/thread.h>
 #include "tinyxml.h"
 #include "tinystr.h"
