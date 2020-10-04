71b2df7e931c67123d596ea63030e2e557b89488
Alec Leamas <leamas.alec@nowhere.net>
2020-10-04 19:17:59 +0200

    ci: macos: Cleanup.

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -7,7 +7,7 @@ set -xe
 
 #
 # Check if the cache is with us. If not, re-install brew.
-brew list --versions cmake || brew update-reset
+brew list --versions libexif || brew update-reset
 
 for pkg in cairo cmake libarchive libexif python wget; do
     brew list --versions $pkg || brew install $pkg || brew install $pkg || :
@@ -16,20 +16,16 @@ done
 
 wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
 tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
-export PATH="/usr/local/opt/gettext/bin:$PATH"
-echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
 
 export MACOSX_DEPLOYMENT_TARGET=10.9
 
 rm -rf build && mkdir build && cd build
 cmake \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
-  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
-  -DCMAKE_INSTALL_PREFIX= \
+  -DCMAKE_INSTALL_PREFIX="/" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
-  "/" \
   ..
-make -sj2 tarball
+make tarball
 make pkg
 
 wget -q http://opencpn.navnux.org/build_deps/Packages.dmg
@@ -37,5 +33,5 @@ hdiutil attach Packages.dmg
 sudo installer -pkg "/Volumes/Packages 1.2.5/Install Packages.pkg" -target "/"
 make create-pkg
 
-
+# Install cloudsmith needed by upload script
 pip3 install cloudsmith-cli
