53b7b93f7adc2d4d5ee7b94a73ca83b9a10637d2
Rasbats <miker@members.swis.net>
2020-10-07 19:20:30 +0100

    v2.5.25

--- cmake/Targets.cmake
+++ cmake/Targets.cmake
@@ -56,7 +56,7 @@ function (tarball_target)
   add_custom_target(tarball-conf)
   add_custom_command(
     TARGET tarball-conf
-    COMMAND cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/app/Contents/files
+    COMMAND cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/app/files
             -DBUILD_TYPE:STRING=tarball ${CMAKE_BINARY_DIR}
   )
   add_custom_target(tarball-build)
