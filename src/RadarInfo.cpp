fcfd4bdb4762950675dd4d46912cfa630221c984
Douwe Fokkema <df@percussion.nl>
2020-12-04 16:28:37 +0100

    Remove compile warning Remove file, not needed.

--- src/RadarInfo.cpp
+++ src/RadarInfo.cpp
@@ -1417,11 +1417,8 @@ void RadarInfo::ClearTrails() {
 
 int RadarInfo::GetNearestRange(int range_meters, int units) {
   const int *ranges;
-  const int *tests;
   size_t count = RadarFactory::GetRadarRanges(this, M_SETTINGS.range_units, &ranges);
- 
   size_t n;
-
   for (n = count - 1; n > 0; n--) {
     if (ranges[n] <= range_meters) {  // step down until past the right range value
       break;
