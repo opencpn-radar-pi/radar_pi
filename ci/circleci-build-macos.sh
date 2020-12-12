1ea979312fe42d49acf23d7a07ce67c5af6f7647
Rasbats <miker@members.swis.net>
2020-12-12 07:08:44 +0000

    Update circleci-build-macos.sh

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -8,14 +8,8 @@ set -xe
 
 #
 # Check if the cache is with us. If not, re-install brew.
-brew list --versions libexif || {
-    brew update-reset
-    # As indicated by warning message in CircleCI build log:
-    git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core" \
-        fetch --unshallow
-    git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask" \
-        fetch --unshallow
-}
+brew list --versions libexif || brew update-reset
+
 
 for pkg in cairo cmake gettext libarchive libexif python wget; do
     brew list --versions $pkg || brew install $pkg || brew install $pkg || :
@@ -41,7 +35,8 @@ rm -rf build && mkdir build && cd build
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312B_opencpn50_macos109/bin/wx-config \
-  -DCMAKE_INSTALL_PREFIX="/" \
+  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312B_opencpn50_macos109" \
+  -DCMAKE_INSTALL_PREFIX= \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
   ..
 make -j $(sysctl -n hw.physicalcpu) VERBOSE=1 tarball
