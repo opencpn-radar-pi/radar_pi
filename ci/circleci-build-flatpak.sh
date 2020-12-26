6205c183062ac164ca52814511ad3da5f658b93b
Kees Verruijt <kees@verruijt.net>
2020-12-26 13:51:16 -0400

    Copy flatpak files from TideFinder_pi

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -6,42 +6,57 @@
 # container. 
 #
 # flatpak-builder can be run in a docker image. However, this
-# must then be run in privileged mode, which means it we need 
-# a full VM to run it.
+# must then be run in privileged mode, which means we need 
+# a full-fledged VM to run it.
 #
 
-# bailout on errors and echo commands.
-set -euo pipefail
+set -e
+
+MANIFEST=$(cd flatpak; ls org.opencpn.OpenCPN.Plugin*yaml)
+echo "Using manifest file: $MANIFEST"
 set -x
-##sudo apt-get -qq update
 
-DOCKER_SOCK="unix:///var/run/docker.sock"
-if [ -n "${TRAVIS:-}" ]; then
-    TOPDIR=/opencpn-ci
-fi
+# Give the apt update daemons a chance to leave the scene.
+sudo systemctl stop apt-daily.service apt-daily.timer
+sudo systemctl kill --kill-who=all apt-daily.service apt-daily-upgrade.service
+sudo systemctl mask apt-daily.service apt-daily-upgrade.service
+sudo systemctl daemon-reload
 
-if [ -n "${CIRCLECI:-}" ]; then
-   TOPDIR=/root/project
-fi
+sudo add-apt-repository -y ppa:alexlarsson/flatpak
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub \
+    | sudo apt-key add -
+sudo apt update
 
-echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCKER_SOCK -s devicemapper\"" \
-    | sudo tee /etc/default/docker > /dev/null
-sudo service docker restart;
-sleep 5;
-sudo docker pull fedora:28;
-sleep 2
+sudo apt install build-essential flatpak-builder flatpak tar
+flatpak remote-add --user --if-not-exists flathub \
+    https://flathub.org/repo/flathub.flatpakrepo
+flatpak install --user -y flathub org.opencpn.OpenCPN > /dev/null
+flatpak install --user -y flathub org.freedesktop.Sdk//18.08  >/dev/null
+sed -i '/^runtime-version/s/:.*/: stable/' flatpak/$MANIFEST
 
-docker run --privileged -d -ti -e "container=docker"  \
-    -e TOPDIR=$TOPDIR \
-    -v /sys/fs/cgroup:/sys/fs/cgroup \
-    -v $(pwd):$TOPDIR:rw \
-    fedora:28   /usr/sbin/init
-DOCKER_CONTAINER_ID=$(docker ps | grep fedora | awk '{print $1}')
-docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "bash -xe $TOPDIR/ci/docker-build-flatpak.sh 28;
-         echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
-docker ps -a
-docker stop $DOCKER_CONTAINER_ID
-docker rm -v $DOCKER_CONTAINER_ID
+# The flatpak checksumming needs python3:
+pyenv local $(pyenv versions | sed 's/*//' | awk '{print $1}' | tail -1)
+cp .python-version $HOME
 
+mkdir build; cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j $(nproc) VERBOSE=1 flatpak
+
+# Restore file so the cache checksumming is ok.
+git checkout ../flatpak/$MANIFEST
+
+# Wait for apt-daily to complete, install cloudsmith-cli required by upload.sh.
+# apt-daily should not restart, it's masked.
+# https://unix.stackexchange.com/questions/315502
+echo -n "Waiting for apt_daily lock..."
+sudo flock /var/lib/apt/daily_lock echo done
+
+# Install cloudsmith, requiered by upload script
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user cloudsmith-cli
+
+# Required by git-push
+python3 -m pip install --user cryptography
+
+# python install scripts in ~/.local/bin:
+echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.uploadrc
