3e6196849c75f775e34b7e0ac9920c3edaf99735
Alec Leamas <leamas.alec@nowhere.nt>
2021-01-20 20:50:44 +0100

    ci: Use pypi's updated cmake for debian builds.

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -17,13 +17,13 @@ if [ -n "$BUILD_GTK3" ]; then
         /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
 fi
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j $(nproc) VERBOSE=1 tarball
-
 sudo apt-get install \
     python3-pip python3-setuptools python3-dev python3-wheel \
     build-essential libssl-dev libffi-dev 
 
 # Latest pip 21.0.0 is broken:
 python3 -m pip install --force-reinstall pip==20.3.4
-python3 -m pip install --user -q cloudsmith-cli cryptography 
+python3 -m pip install --user -q cloudsmith-cli cryptography cmake
+
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j $(nproc) VERBOSE=1 tarball
