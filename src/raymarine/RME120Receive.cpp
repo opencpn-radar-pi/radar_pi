2976776cdea8eee2107fbe3a69f415876869e37d
Douwe Fokkema <df@percussion.nl>
2021-04-09 17:18:00 +0200

    Ranges working for Quantum, radar state working, radar window functioning

--- src/raymarine/RME120Receive.cpp
+++ src/raymarine/RME120Receive.cpp
@@ -319,7 +319,7 @@ void RME120Receive::ProcessFrame(const UINT8 *data, size_t len) {  // This is th
         m_ri->m_data_timeout = now + DATA_TIMEOUT + 100; //$$$
         break;
       case 0x00280002:
-        ProcessQuantumScanData(data, len);
+        ProcessQuantumReport(data, len);
         m_ri->m_data_timeout = now + DATA_TIMEOUT + 100;  //$$$
         break;
       case 0x00010006:
@@ -390,6 +390,52 @@ struct RMRadarReport {
   uint8_t mbs_enabled;  // Main Bang Suppression enabled if 1
 };
 
+struct QuantumRadarReport {
+
+  uint8_t unknown[4];   // 0
+  uint8_t status;       // 4
+  uint8_t unknown2[15]; // 5
+  uint8_t range_index;  // 20
+  uint8_t unknown3[127];
+  uint32_t ranges[20]; // 148
+
+  //uint32_t field01;       // 0x010001  // 0-3
+  //uint32_t ranges[11];    // 4 - 47
+  //uint32_t fieldx_1[33];  // 48 -
+
+  //uint8_t status;       // 2 - warmup, 1 - transmit, 0 - standby, 6 - shutting down (warmup time - countdown), 3 - shutdown  // 180
+  //uint8_t fieldx_2[3];  // 181
+  //uint8_t warmup_time;  // 184
+  //uint8_t signal_strength;  // number of bars   // 185
+
+  //uint8_t fieldx_3[7];  // 186
+  //uint8_t range_id;     // 193
+  //uint8_t fieldx_4[2];  // 194
+  //uint8_t auto_gain;    // 196
+
+  //uint8_t fieldx_5[3];   // 197
+  //uint32_t gain;         // 200
+  //uint8_t auto_sea;      // 0 - disabled; 1 - harbour, 2 - offshore, 3 - coastal   // 204
+  //uint8_t fieldx_6[3];   // 205
+  //uint8_t sea_value;     // 208
+  //uint8_t rain_enabled;  // 209
+  //uint8_t fieldx_7[3];   // 210
+  //uint8_t rain_value;    // 213
+  //uint8_t ftc_enabled;   // 214
+  //uint8_t fieldx_8[3];   // 215
+  //uint8_t ftc_value;     // 218
+  //uint8_t auto_tune;
+  //uint8_t fieldx_9[3];
+  //uint8_t tune;
+  //int16_t bearing_offset;  // degrees * 10; left - negative, right - positive
+  //uint8_t interference_rejection;
+  //uint8_t fieldx_10[3];
+  //uint8_t target_expansion;
+  //uint8_t fieldx_11[13];
+  //uint8_t mbs_enabled;  // Main Bang Suppression enabled if 1
+};
+
+
 struct RMRadarFixedReport {
   uint32_t field01;           // 0x010002
   uint8_t fieldx_1[213];      // 221 - magnetron current; 233, 234 - rotation time ms (251 total) // 4
@@ -441,6 +487,146 @@ void RME120Receive::logBinaryData(const wxString &what, const uint8_t *data, int
   LOG_INFO(explain);
 }
 
+void RME120Receive::ProcessQuantumReport(const UINT8 *data, int len) {
+  QuantumRadarReport *bl_pter = (QuantumRadarReport *)data;
+  wxString s;
+   logBinaryData(wxT("ProcessQuantumReport"), data, len);
+
+    switch (bl_pter->status) {
+      case 0:
+        LOG_RECEIVE(wxT("%s received transmit off from %s"), m_ri->m_name.c_str(), "--" /*addr.c_str()*/);
+        m_ri->m_state.Update(RADAR_STANDBY);
+        break;
+      case 1:
+        LOG_RECEIVE(wxT("%s received transmit on from %s"), m_ri->m_name.c_str(), "--" /*addr.c_str()*/);
+        m_ri->m_state.Update(RADAR_TRANSMIT);
+        break;
+      case 2:  // Warmup, but don't know if Quantum has these states
+        LOG_RECEIVE(wxT("%s radar is warming up %s"), m_ri->m_name.c_str(), "--" /*addr.c_str()*/);
+        m_ri->m_state.Update(RADAR_WARMING_UP);
+        break;
+      case 3:  // Off
+        LOG_RECEIVE(wxT("%s radar is off %s"), m_ri->m_name.c_str(), "--" /*addr.c_str()*/);
+        m_ri->m_state.Update(RADAR_OFF);
+        break;
+      default:
+        m_ri->m_state.Update(RADAR_STANDBY);
+        break;
+    }
+
+
+   for (int i = 0; i < 20; i++) {
+     m_ri->m_radar_ranges[i] = (int)(1.852 * (double)bl_pter->ranges[i]);
+     LOG_RECEIVE(wxT("received range= %i, radar_ranges=  %d "), bl_pter->ranges[i], m_ri->m_radar_ranges[i]);
+   }
+
+    if (bl_pter->ranges[2] == 125) {
+      M_SETTINGS.range_units = RANGE_NAUTIC;  // We don't know yet how Raymarine switches range units
+    } else if (bl_pter->ranges[0] == 135) {   // Raymarine has no RANGE_MIXED
+                                              // Ray marine alse has units statue miles, not supported by radar_pi
+      M_SETTINGS.range_units = RANGE_METRIC;
+    } else {
+      LOG_INFO(wxT("Other range units found, bl_pter->ranges[0]= %i"), bl_pter->ranges[0]);
+      M_SETTINGS.range_units = RANGE_NAUTIC;  // to be adapted to RANGE_MIXED if that exists with Raymarine
+    }
+
+    
+    int range_id = bl_pter->range_index;
+    LOG_INFO(wxT("$$$ range index=%i, range = %i"), range_id, m_ri->m_radar_ranges[range_id]);
+    if ((m_ri->m_radar_ranges[range_id] * 2) != m_range_meters) {
+      if (m_pi->m_settings.verbose >= 1) {
+        LOG_RECEIVE(wxT("%s now scanning with range %d meters (was %d meters)"), m_ri->m_name.c_str(),
+                    m_ri->m_radar_ranges[range_id] * 2, m_range_meters);
+      }
+      if (m_ri->m_radar_ranges[range_id] > 0) {
+        m_range_meters = m_ri->m_radar_ranges[range_id] * 2;  // displayed values are half of scanned values
+      m_updated_range = true;
+      m_ri->m_range.Update(m_range_meters / 2);  // RM MFD shows half of what is received
+      LOG_RECEIVE(wxT("m_range updated to %i"), (m_range_meters / 2));
+      } else {
+        m_range_meters = 1; // prevent accidents down the road
+        return;
+      }
+    }
+
+  // /* RadarControlState state;
+  //  state = (bl_pter->auto_gain > 0) ? RCS_AUTO_1 : RCS_MANUAL;
+  //  m_ri->m_gain.TransformAndUpdate(bl_pter->gain);
+  //  m_ri->m_gain.UpdateState(state);
+  //  LOG_RECEIVE(wxT("gain updated received1= %i, displayed = %i"), bl_pter->gain, m_ri->m_gain.GetValue());
+
+  //  state = (RadarControlState)bl_pter->auto_sea;
+  //  m_ri->m_sea.TransformAndUpdate(bl_pter->sea_value);
+  //  m_ri->m_sea.UpdateState(state);
+  //  LOG_RECEIVE(wxT("sea updated received= %i, displayed = %i, state=%i"), bl_pter->sea_value, m_ri->m_sea.GetValue(), state);
+
+  //  state = (bl_pter->rain_enabled) ? RCS_MANUAL : RCS_OFF;
+  //  LOG_RECEIVE(wxT("rain state=%i bl_pter->rain_enabled=%i"), state, bl_pter->rain_enabled);
+  //  m_ri->m_rain.TransformAndUpdate(bl_pter->rain_value);
+  //  m_ri->m_rain.UpdateState(state);
+  //  LOG_RECEIVE(wxT("rain updated received= %i, displayed = %i state=%i"), bl_pter->rain_value, m_ri->m_rain.GetValue(), state);
+
+  //  state = (bl_pter->ftc_enabled) ? RCS_MANUAL : RCS_OFF;
+  //  m_ri->m_ftc.TransformAndUpdate(bl_pter->ftc_value);
+  //  m_ri->m_ftc.UpdateState(state);
+  //  LOG_RECEIVE(wxT("ftc updated received= %i, displayed = %i state=%i"), bl_pter->ftc_value, m_ri->m_ftc.GetValue(), state);
+
+  //  m_ri->m_target_expansion.Update(bl_pter->target_expansion);
+  //  m_ri->m_interference_rejection.Update(bl_pter->interference_rejection);
+
+  //  int ba = (int)bl_pter->bearing_offset;
+  //  m_ri->m_bearing_alignment.Update(ba);
+
+  //  state = (bl_pter->auto_tune > 0) ? RCS_AUTO_1 : RCS_MANUAL;
+  //  m_ri->m_tune_fine.Update(bl_pter->tune, state);
+
+  //  state = (bl_pter->auto_tune > 0) ? RCS_AUTO_1 : RCS_MANUAL;
+
+  //  m_ri->m_tune_coarse.UpdateState(state);
+
+  //  state = (bl_pter->mbs_enabled > 0) ? RCS_MANUAL : RCS_OFF;
+  //  m_ri->m_main_bang_suppression.Update(bl_pter->mbs_enabled, RCS_MANUAL);
+
+  //  m_ri->m_warmup_time.Update(bl_pter->warmup_time);
+  //  m_ri->m_signal_strength.Update(bl_pter->signal_strength);
+  //}*/
+
+  int status = m_ri->m_state.GetValue();
+  //wxString stat;
+  //switch (status) {
+  //  case RADAR_OFF:
+  //    LOG_VERBOSE(wxT("%s reports status RADAR_OFF"), m_ri->m_name.c_str());
+  //    stat = _("Off");
+  //    break;
+
+  //  case RADAR_STANDBY:
+  //    LOG_VERBOSE(wxT("%s reports status STANDBY"), m_ri->m_name.c_str());
+  //    stat = _("Standby");
+  //    break;
+
+  //  case RADAR_WARMING_UP:
+  //    LOG_VERBOSE(wxT("%s reports status RADAR_WARMING_UP"), m_ri->m_name.c_str());
+  //    stat = _("Warming up");
+  //    break;
+
+  //  case RADAR_TRANSMIT:
+  //    LOG_VERBOSE(wxT("%s reports status RADAR_TRANSMIT"), m_ri->m_name.c_str());
+  //    stat = _("Transmit");
+  //    break;
+
+  //  default:
+  //    // LOG_BINARY_RECEIVE(wxT("received unknown radar status"), report, len);
+  //    stat = _("Unknown status");
+  //    break;
+  //}
+
+  //s = wxString::Format(wxT("IP %s %s"), m_ri->m_radar_address.FormatNetworkAddress(), stat.c_str());
+
+  //RadarLocationInfo info = m_ri->GetRadarLocationInfo();
+  //s << wxT("\n") << _("IF-Serial #") << info.serialNr;
+  //SetInfoStatus(s);
+}
+
 void RME120Receive::ProcessRMReport(const UINT8 *data, int len) {
   RMRadarReport *bl_pter = (RMRadarReport *)data;
   wxString s;
@@ -481,7 +667,7 @@ void RME120Receive::ProcessRMReport(const UINT8 *data, int len) {
 
     for (int i = 0; i < 11; i++) {
       m_ri->m_radar_ranges[i] = (int)(1.852 * (double)bl_pter->ranges[i]);
-      LOG_RECEIVE(wxT("received range= %i, radar_ranges=  %d "), bl_pter->ranges[i], m_ri->m_radar_ranges[i]);
+      //LOG_RECEIVE(wxT("received range= %i, radar_ranges=  %d "), bl_pter->ranges[i], m_ri->m_radar_ranges[i]);
     }
     int range_id;
     if (HDtype) {
@@ -489,7 +675,7 @@ void RME120Receive::ProcessRMReport(const UINT8 *data, int len) {
     } else {
       range_id = bl_pter->range_id;
     }
-    
+
     if ((m_ri->m_radar_ranges[range_id] * 2) != m_range_meters) {
       if (m_pi->m_settings.verbose >= 1) {
         LOG_RECEIVE(wxT("%s now scanning with range %d meters (was %d meters)"), m_ri->m_name.c_str(),
@@ -497,11 +683,11 @@ void RME120Receive::ProcessRMReport(const UINT8 *data, int len) {
       }
       if (m_ri->m_radar_ranges[range_id] > 0) {
         m_range_meters = m_ri->m_radar_ranges[range_id] * 2;  // displayed values are half of scanned values
-      m_updated_range = true;
-      m_ri->m_range.Update(m_range_meters / 2);  // RM MFD shows half of what is received
-      LOG_RECEIVE(wxT("m_range updated to %i"), (m_range_meters / 2));
+        m_updated_range = true;
+        m_ri->m_range.Update(m_range_meters / 2);  // RM MFD shows half of what is received
+        LOG_RECEIVE(wxT("m_range updated to %i"), (m_range_meters / 2));
       } else {
-        m_range_meters = 1; // prevent accidents down the road
+        m_range_meters = 1;  // prevent accidents down the road
         return;
       }
     }
@@ -583,6 +769,7 @@ void RME120Receive::ProcessRMReport(const UINT8 *data, int len) {
   s << wxT("\n") << _("IF-Serial #") << info.serialNr;
   SetInfoStatus(s);
 }
+
 
 void RME120Receive::ProcessFixedReport(const UINT8 *data, int len) {
   if (len == sizeof(RMRadarFixedReport)) {
@@ -853,7 +1040,7 @@ void RME120Receive::ProcessQuantumScanData(const UINT8 *data, int len) {
     // return;  $$$
   }
   QuantumHeader *qheader = (QuantumHeader *)data;
-  logBinaryData(wxT("Scandata_x"), data, len);
+  //logBinaryData(wxT("Scandata_x"), data, len);
   if (len > (int)(sizeof(Header1) /* + sizeof(Header3)*/)) {
     Header1 *pHeader = (Header1 *)data;
     bool HDtype = true;
@@ -936,7 +1123,7 @@ void RME120Receive::ProcessQuantumScanData(const UINT8 *data, int len) {
         LOG_INFO(wxT("$$$ returns_per_line too large %i"), returns_per_line);
         }
 
-       LOG_BINARY_RECEIVE(wxT("spoke data dData"), unpacked_data, idorg);
+       //LOG_BINARY_RECEIVE(wxT("spoke data dData"), unpacked_data, idorg);
       dataPtr = unpacked_data;
 
       /*nextOffset += pSData->length;*/
