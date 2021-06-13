f61acf00cde41dc6c1124319efbc898b42cfd6f4
Alec Leamas <leamas.alec@nowhere.nt>
2021-06-13 19:09:30 +0200

    ci: Update flatpak builds (#175).

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -9,29 +9,13 @@ MANIFEST=$(cd flatpak; ls org.opencpn.OpenCPN.Plugin*yaml)
 echo "Using manifest file: $MANIFEST"
 set -x
 
-# On old systems: give the apt update daemons a chance to leave the scene
-# while we build.
-if systemctl status apt-daily-upgrade.service &> /dev/null; then
-    sudo systemctl stop apt-daily.service apt-daily.timer
-    sudo systemctl kill --kill-who=all \
-        apt-daily.service apt-daily-upgrade.service
-    sudo systemctl mask apt-daily.service apt-daily-upgrade.service
-    sudo systemctl daemon-reload
-fi
-
-# Install the flatpak PPA so we can access a usable flatpak version
-# (not available on Ubuntu Xenial 16.04)
-if [ -n "$USE_CUSTOM_PPA" ]; then
-    sudo add-apt-repository -y ppa:alexlarsson/flatpak
-    wget -q -O - https://dl.google.com/linux/linux_signing_key.pub \
-        | sudo apt-key add -
-fi
 sudo apt update
 
+
+# Install flatpak and flatpak-builder
 sudo apt install flatpak flatpak-builder
 flatpak remote-add --user --if-not-exists \
     flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
 
 # For now, horrible hack: aarch 64 builds are using the updated runtime
 # 20.08 and the opencpn beta version using old 18.08 runtime.
@@ -59,10 +43,6 @@ sed -i "/^runtime-version/s/:.*/: $FLATPAK_BRANCH/" flatpak/$MANIFEST
 pyenv local $(pyenv versions | sed 's/*//' | awk '{print $1}' | tail -1)
 cp .python-version $HOME
 
-# Install a recent cmake, ubuntu 16.04 is too old.
-export PATH=$HOME/.local/bin:$PATH
-python -m pip install --user cmake
-
 # Configure and build the plugin tarball and metadata.
 mkdir build; cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
@@ -71,17 +51,9 @@ make -j $(nproc) VERBOSE=1 flatpak
 # Restore patched file so the cache checksumming is ok.
 git checkout ../flatpak/$MANIFEST
 
-# Wait for apt-daily to complete. apt-daily should not restart, it's masked.
-# https://unix.stackexchange.com/questions/315502
-echo -n "Waiting for apt_daily lock..."
-sudo flock /var/lib/apt/daily_lock echo done
-
-# Install cloudsmith, required by upload script
-python3 -m pip install --user --upgrade pip
-python3 -m pip install --user cloudsmith-cli
-
-# Required by git-push
-python3 -m pip install --user cryptography
+# Install cloudsmith and cryptography, required by upload script and git-push
+python3 -m pip install -q --user --upgrade pip
+python3 -m pip install -q --user cloudsmith-cli cryptography
 
 # python install scripts in ~/.local/bin, teach upload.sh to use this in PATH
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.uploadrc
