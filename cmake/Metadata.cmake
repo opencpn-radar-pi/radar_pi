7872a3cef3c15e401dbe2b5da59f2af20347363b
Rasbats <miker@members.swis.net>
2021-07-05 15:26:06 +0100

    v2.7.12.0-beta

--- cmake/Metadata.cmake
+++ cmake/Metadata.cmake
@@ -23,7 +23,7 @@ execute_process(
 )
 
 execute_process(
-  COMMAND git tag --contains HEAD
+  COMMAND git log -1 --format=%s
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   OUTPUT_VARIABLE _git_tag
   RESULT_VARIABLE error_code
