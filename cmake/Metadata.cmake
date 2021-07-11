ad18700a54d356379499a6f480fe43802f1ade1f
Kees Verruijt <kees@verruijt.net>
2021-07-11 12:52:35 +0000

    Use git commit ID instead of version

--- cmake/Metadata.cmake
+++ cmake/Metadata.cmake
@@ -80,7 +80,7 @@ if (_pre_rel MATCHES "^[^-].")
   # Prepend - if pre-release not empty and doesn't start with -
   string(PREPEND _pre_rel "-")
 endif ()
-set(pkg_semver "${PROJECT_VERSION}${_pre_rel}+${_build_id}.${_gitversion}")
+set(pkg_semver "${PROJECT_VERSION}${_pre_rel}+${_build_id}.${_git_tag}")
 
 # pkg_displayname: Used for xml metadata and GUI name
 if (ARCH MATCHES "arm64|aarch64")
