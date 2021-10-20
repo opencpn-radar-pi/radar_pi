6b47179c56416aa3280f93b37eae5398e54a2a13
Alec Leamas <leamas.alec@nowhere.net>
2021-10-20 16:39:29 +0200

    win_deps.bat: Enhance installation corner cases.
    choco install cmake and python using native installer paths
    requiring running "refreshenv" after installation to update
    %PATH%.
    
    7z and wget installs installation symlinks in
    /Programdata/chocolatey/bin, so here no new PATH is required.

--- buildwin/win_deps.bat
+++ buildwin/win_deps.bat
@@ -1,14 +1,22 @@
 rem
 rem Install build dependencies. Requires a working choco installation,
-rem see https://docs.chocolatey.org/en-us/choco/setup. 
+rem see https://docs.chocolatey.org/en-us/choco/setup.
 rem
-rem Initial run will do choco installs requiring administrative 
+rem Initial run will do choco installs requiring administrative
 rem privileges.
 rem
 
 
-SET PATH=C:\Program Files\CMake\bin;C:\Python38;C:\Python38\Scripts;%PATH%
-python --version > nul 2>&1 || choco install -y python
+python --version > nul 2>&1 && python -m ensurepip > nul 2>&1
+if errorlevel 1 (
+   choco install -y python
+   refreshenv
+)
+cmake --version > nul 2>&1
+if errorlevel 1 (
+   choco install -y cmake
+   refreshenv
+)
 python --version
 python -m ensurepip
 python -m pip install --upgrade pip
@@ -23,7 +31,7 @@ SET PATH=%PATH%;%WXWIN%;%wxWidgets_LIB_DIR%
 if not exist %WXWIN% (
   wget --version > nul 2>&1 || choco install -y wget
   wget https://download.opencpn.org/s/E2p4nLDzeqx4SdX/download -O wxWidgets-3.1.2.7z
-  7z i > nul 2>&1 || choco install -y 7z
+  7z i > nul 2>&1 || choco install -y 7zip
   7z x wxWidgets-3.1.2.7z -o%WXWIN%
 )
 
