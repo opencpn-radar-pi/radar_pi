248dfd301e1273c2a5f9c8ca7db241a1e047fcae
Rasbats <miker@members.swis.net>
2021-10-26 11:56:19 +0100

    Revert "Remove Android gesture events"
    This reverts commit 4ddc942e34268405d184c50c2bc1f7390e3619bb.

--- src/ShipDrivergui_impl.h
+++ src/ShipDrivergui_impl.h
@@ -24,6 +24,9 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************
  */
+#ifdef __OCPN__ANDROID__
+#include <wx/qt/private/wxQtGesture.h>
+#endif
 
 #ifndef _HRGUI_IMPL_H_
 #define _HRGUI_IMPL_H_
@@ -108,6 +111,8 @@ public:
 
 #ifdef __OCPN__ANDROID__
     void OnMouseEvent( wxMouseEvent& event );
+    void OnEvtPinchGesture( wxQT_PinchGestureEvent &event);
+    void OnEvtPanGesture( wxQT_PanGestureEvent &event);
 #endif
 
     wxString createVHWSentence(double stw, double hdg);
