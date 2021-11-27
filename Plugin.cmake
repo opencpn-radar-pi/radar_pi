88d3e471b2937e5d1b63d3f039b1973922346a2f
Rasbats <miker@members.swis.net>
2021-11-27 15:00:12 +0000

    sd3.0.0

--- Plugin.cmake
+++ Plugin.cmake
@@ -35,7 +35,7 @@ option(SHIPDRIVER_USE_SVG "Use SVG graphics" ON)
 #
 set(PKG_NAME ShipDriver_pi)
 set(PKG_VERSION  3.0.0)
-set(PKG_PRERELEASE "beta2")  # Empty, or a tag like 'beta'
+set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME ShipDriver)    # Dialogs, installer artifacts, ...
 set(PLUGIN_API_NAME ShipDriver) # As of GetCommonName() in plugin API
