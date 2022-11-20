ddeeb34b949a7d2390b9f92f4676cad98f67a9e6
Alec Leamas <leamas.alec@nowhere.net>
2022-11-21 00:18:39 +0100

    ci: Add bullseye-armhf build.

--- ci/circleci-build-debian-armhf.sh
+++ ci/circleci-build-debian-armhf.sh
@@ -25,6 +25,48 @@ cd $ci_source
 git submodule update --init opencpn-libs
 
 cat > $ci_source/build.sh << "EOF"
+function remove_wx30() {
+  apt remove -y \
+      libwxsvg3 \
+      wx3.0-i18n \
+      wx-common \
+      libwxgtk3.0-gtk3-0v5 \
+      libwxbase3.0-0v5 \
+      libwxsvg3 wx3.0-i18n \
+      wx-common \
+      libwxgtk3.0-gtk3-0v5 \
+      libwxbase3.0-0v5 wx3.0-headers \
+      libwxsvg3 \
+      libwxsvg-dev
+}
+
+# Install generated packages
+function install_wx32() {
+  test -d /usr/local/pkg || mkdir /usr/local/pkg
+  chmod a+w /usr/local/pkg
+  repo="https://dl.cloudsmith.io/public/alec-leamas/wxwidgets"
+  head="deb/debian/pool/bullseye/main"
+  vers="3.2.1+dfsg-1~bpo11+1"
+  pushd /usr/local/pkg
+  wget  $repo/$head/w/wx/wx3.2-i18n_${vers}/wx3.2-i18n_${vers}_all.deb
+  wget  $repo/$head/w/wx/wx3.2-headers_${vers}/wx3.2-headers_${vers}_all.deb
+  wget  $repo/$head/w/wx/wx-common_3.2.1+dfsg-1~bpo11+1/wx-common_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxgtk-webview3.2-dev_3.2.1+dfsg-1~bpo11+1/libwxgtk-webview3.2-dev_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxgtk-webview3.2-0_3.2.1+dfsg-1~bpo11+1/libwxgtk-webview3.2-0_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxgtk-media3.2-dev_3.2.1+dfsg-1~bpo11+1/libwxgtk-media3.2-dev_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxgtk3.2-dev_3.2.1+dfsg-1~bpo11+1/libwxgtk3.2-dev_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxgtk3.2-0_3.2.1+dfsg-1~bpo11+1/libwxgtk3.2-0_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxbase3.2-0_3.2.1+dfsg-1~bpo11+1/libwxbase3.2-0_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxgtk-media3.2-dev_3.2.1+dfsg-1~bpo11+1/libwxgtk-media3.2-dev_3.2.1+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxsvg-dev_2:1.5.23+dfsg-1~bpo11+1/libwxsvg-dev_1.5.23+dfsg-1~bpo11+1_armhf.deb
+  wget  $repo/$head/l/li/libwxsvg3_2:1.5.23+dfsg-1~bpo11+1/libwxsvg3_1.5.23+dfsg-1~bpo11+1_armhf.deb
+
+  dpkg -i --force-depends $(ls /usr/local/pkg/*deb)
+  sed -i '/^user_mask_fits/s|{.*}|{ /bin/true; }|' \
+      /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.2
+  popd
+}
+
 
 set -x
 
@@ -43,15 +85,13 @@ elif [ "$debian_rel" = bullseye ]; then
       >> /etc/apt/sources.list
     apt update
     apt install -y cmake/bullseye-backports
-elif [ "$debian_rel" = buster ]; then
-    url='https://dl.cloudsmith.io/public/alec-leamas/opencpn-plugins-stable'
-    wget --no-verbose \
-        $url/deb/debian/pool/buster/main/c/cm/cmake-data_3.19.3-0.1_all.deb
-    wget --no-verbose \
-        $url/deb/debian/pool/buster/main/c/cm/cmake_3.19.3-0.1_armhf.deb
-    apt install -y ./cmake_3.19.3-0.1_armhf.deb ./cmake-data_3.19.3-0.1_all.deb
 else
     echo "Unknown debian release: $debian_rel"
+fi
+
+if [ -n "@BUILD_WX32@" ]; then
+  remove_wx30
+  install_wx32
 fi
 
 cd /ci-source
@@ -63,6 +103,7 @@ chown --reference=.. .
 EOF
 
 sed -i "s/@TARGET_TUPLE@/$TARGET_TUPLE/" $ci_source/build.sh
+sed -i "s/@BUILD_WX32@/$BUILD_WX32/" $ci_source/build.sh
 
 
 # Run script in docker image
