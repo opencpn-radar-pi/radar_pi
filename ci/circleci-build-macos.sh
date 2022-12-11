6daa5438e1efbfe1323650e111e38d730a588d30
Alec Leamas <leamas.alec@nowhere.net>
2022-12-11 22:42:58 +0100

    ci: macos: Use homebrew wxwidgets 3.2 package

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -43,18 +43,10 @@ done
 
 export OPENSSL_ROOT_DIR='/usr/local/opt/openssl'
 
-# Install the pre-built wxWidgets package
-
-wget -q https://download.opencpn.org/s/MCiRiq4fJcKD56r/download \
-    -O /tmp/wx315_opencpn50_macos1010.tar.xz
-tar -C /tmp -xJf /tmp/wx315_opencpn50_macos1010.tar.xz
-
 # Build and package
 cd build-osx
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx315_opencpn50_macos1010/bin/wx-config \
-  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx315_opencpn50_macos1010" \
   -DCMAKE_INSTALL_PREFIX= \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
   ..
