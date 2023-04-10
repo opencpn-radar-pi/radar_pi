005fc9d930a094f091e25dbf770da4bd8ff575b2
Kees Verruijt <kees@verruijt.net>
2023-04-10 15:09:37 +0200

    And another attempt

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -44,6 +44,7 @@ function install_wx32() {
   wget -q $repo/$head/l/li/libwxgtk-media3.2-dev_${vers}/libwxgtk-media3.2-dev_${vers}_amd64.deb
   wget -q $repo/$head/l/li/libwxgtk3.2-dev_${vers}/libwxgtk3.2-dev_${vers}_amd64.deb
   wget -q $repo/$head/l/li/libwxgtk3.2-1_${vers}/libwxgtk3.2-1_${vers}_amd64.deb
+  wget -q $repo/$head/l/li/libwxgtk-gl3.2-1_${vers}/libwxgtk-gl3.2-1_${vers}_amd64.deb
   wget -q $repo/$head/l/li/libwxbase3.2-1_${vers}/libwxbase3.2-1_${vers}_amd64.deb
   wget -q $repo/$head/l/li/libwxgtk-media3.2-1_${vers}/libwxgtk-media3.2-1_${vers}_amd64.deb
   #wget -q $repo/$head/l/li/libwxsvg-dev_2:1.5.23+dfsg-1~bpo11+1/libwxsvg-dev_1.5.23+dfsg-1~bpo11+1_amd64.deb
@@ -53,10 +54,10 @@ function install_wx32() {
   sudo sed -i '/^user_mask_fits/s|{.*}|{ /bin/true; }|' \
       /usr/lib/$(uname -m)-linux-gnu/wx/config/gtk3-unicode-3.2
   # See wxWidgets#22790. To be removed after wxw 3.2.3
-  cd /usr/include/wx-3.2/wx/
-  sudo patch -p1 \
-    < $here/../build-deps/0001-matrix.h-Patch-attributes-handling-wxwidgets-22790.patch
-  popd
+  #cd /usr/include/wx-3.2/wx/
+  #sudo patch -p1 \
+  #  < $here/../build-deps/0001-matrix.h-Patch-attributes-handling-wxwidgets-22790.patch
+  #popd
 }
 
 set -xe
