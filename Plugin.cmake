821e8a74caffb6146eebc54a85c686472204be67
Kees Verruijt <kees@verruijt.net>
2023-04-16 17:16:44 +0000

    Bump version number to 5.5.1-alpha

--- Plugin.cmake
+++ Plugin.cmake
@@ -33,8 +33,8 @@ set(OCPN_RELEASE_REPO
 # -------  Plugin setup --------
 #
 set(PKG_NAME radar_pi)
-set(PKG_VERSION 5.5.0)
-set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
+set(PKG_VERSION 5.5.1)
+set(PKG_PRERELEASE "-alpha")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME radar)    # Dialogs, installer artifacts, ...
 set(PLUGIN_API_NAME Radar) # As of GetCommonName() in plugin API
