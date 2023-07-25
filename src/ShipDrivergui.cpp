3ca9b8060f90e09a2a49bd669efd6f53cf427713
Rasbats <miker@members.swis.net>
2023-07-25 08:12:40 +0100

    Remove fixed size from dialog

--- src/ShipDrivergui.cpp
+++ src/ShipDrivergui.cpp
@@ -11,7 +11,7 @@
 
 ShipDriverBase::ShipDriverBase( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
 {
-	this->SetSizeHints( wxSize( 300,250 ), wxSize( -1,-1 ) );
+	this->SetSizeHints( wxSize( -1,-1 ), wxSize( -1,-1 ) );
 	this->SetFont( wxFont( 10, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, wxT("Arial") ) );
 	this->SetBackgroundColour( wxColour( 201, 201, 201 ) );
 
@@ -68,7 +68,7 @@ ShipDriverBase::ShipDriverBase( wxWindow* parent, wxWindowID id, const wxString&
 
 	bSizer131->Add( m_textCtrlRudderPort, 0, wxALIGN_CENTER_VERTICAL|wxALIGN_RIGHT, 0 );
 
-	m_buttonMid = new wxButton( this, wxID_ANY, "|", wxDefaultPosition, wxDefaultSize, 0 );
+	m_buttonMid = new wxButton( this, wxID_ANY, _("|"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_buttonMid->SetFont( wxFont( 11, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, wxT("Arial") ) );
 	m_buttonMid->SetBackgroundColour( wxColour( 250, 203, 107 ) );
 	m_buttonMid->SetToolTip( _("Midships") );
