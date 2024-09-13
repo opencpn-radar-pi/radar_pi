8167ed97db08472e7463c42703260a2724dc7b1d
Alec Leamas <leamas.alec@gmail.com>
2024-09-13 09:33:31 +0200

    Aismaker.h: no using namespace std in header!

--- src/AisMaker.h
+++ src/AisMaker.h
@@ -10,20 +10,18 @@
 #include <wx/textfile.h>
 #include <wx/wx.h>
 
-using namespace std;
-
 class AisMaker {
 
 public:
-    string Str2Str(string str, const char* charsToRemove);
-    float Str2Float(string str, const char* exc);
-    int Str2Int(string str, const char* exc);
-    string Int2BString(int value, int length);
+    std::string Str2Str(std::string str, const char* charsToRemove);
+    float Str2Float(std::string str, const char* exc);
+    int Str2Int(std::string str, const char* exc);
+    std::string Int2BString(int value, int length);
     int findIntFromLetter(char letter);
     char findCharFromNumber(int mp);
-    string Str2Six(string str, int length);
+    std::string Str2Six(std::string str, int length);
     int BString2Int(char* bitlist);
-    string NMEAencapsulate(string BigString, int numsixes);
+    std::string NMEAencapsulate(std::string BigString, int numsixes);
     wxString makeCheckSum(wxString mySentence);
     wxString nmeaEncode(wxString type, int MMSI, wxString status, double spd,
         double ilat, double ilon, double crse, double hdg, wxString channel,
