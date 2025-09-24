23782a6cdc3597e54bd0015c44eb184a920eed12
Douwe <df@percussion.nl>
2025-09-24 10:46:13 +0200

    For dual overlay both radars TRANSMIT

--- src/radar_pi.cpp
+++ src/radar_pi.cpp
@@ -11,7 +11,8 @@
  ***************************************************************************
  *   Copyright (C) 2010 by David S. Register              bdbcat@yahoo.com *
  *   Copyright (C) 2012-2013 by Dave Cowell                                *
- *   Copyright (C) 2012-2016 by Kees Verruijt         canboat@verruijt.net *
+ *   Copyright (C) 2012-2025 by Kees Verruijt         canboat@verruijt.net *
+ *   Copyright (C) 2014-2025 by Douwe Fokkema             df@percussion.nl *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
