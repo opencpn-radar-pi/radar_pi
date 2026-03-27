6efcc4a6c6eff8006604fa6993a51a14b58a9434
Kees Verruijt <kees@verruijt.net>
2026-03-27 20:35:03 +0100

    Templates: Updating buildwin

--- buildwin/win_deps.bat
+++ buildwin/win_deps.bat
@@ -33,7 +33,9 @@ pathman add "%CMAKE_HOME%\bin" > nul
 :: Install choco poedit and add it's persistent user path element
 ::
 set POEDIT_HOME=C:\Program Files (x86)\Poedit\Gettexttools
-if not exist "%POEDIT_HOME%" choco install --no-progress -y poedit
+if not exist "%POEDIT_HOME%" (
+    choco install --version 2.4.2 --no-progress -y poedit
+)
 pathman add "%POEDIT_HOME%\bin" > nul
 
 :: Update required python stuff
