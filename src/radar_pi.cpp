822ba8c7d845c382b338cec5813409bb688e4d90
Kees Verruijt <kees@verruijt.net>
2026-04-09 11:32:28 +0200

    Prevent OpenGL errors by holding off doing OpenGL work until OpenCPN is fully initialized. Fixes #297.

--- src/radar_pi.cpp
+++ src/radar_pi.cpp
@@ -1357,7 +1357,7 @@ wxGLContext* radar_pi::GetChartOpenGLContext() { return m_opencpn_gl_context; }
 //**************************************************************************************************
 
 bool radar_pi::RenderOverlay(wxDC& dc, PlugIn_ViewPort* vp) {
-  if (!m_initialized) {
+  if (!m_initialized || !m_late_init_done) {
     return true;
   }
 
@@ -1371,6 +1371,11 @@ bool radar_pi::RenderOverlay(wxDC& dc, PlugIn_ViewPort* vp) {
 
 bool radar_pi::RenderGLOverlayMultiCanvas(wxGLContext* pcontext, PlugIn_ViewPort* vp, int canvasIndex, int priority) {
   GeoPosition radar_pos;
+
+  if (!m_late_init_done) { // Wait with GL stuff until OpenCPN is done with init of it.
+    return true;
+  }
+
   // prevent this being called recursively
   // no critical section locker (will wait), better to return immediately
   if (m_render_busy) {
