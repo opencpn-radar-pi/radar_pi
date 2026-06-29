7791a11483cb44403354342aa4f3c4b657a11ded
Douwe <df@percussion.nl>
2026-06-29 15:30:06 +0200

    Version to 5.7.1

--- Plugin.cmake
+++ Plugin.cmake
@@ -33,7 +33,7 @@ set(OCPN_RELEASE_REPO
 # -------  Plugin setup --------
 #
 set(PKG_NAME radar_pi)
-set(PKG_VERSION 5.7.0)
+set(PKG_VERSION 5.7.1)
 set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME radar)    # Dialogs, installer artifacts, ...
